97cea3fa4939a91cadfe665474517d96a0e16c5a
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-29 13:05:03 -0400

    The recursive fucntion for factorial works

--- factorial.cpp
+++ factorial.cpp
@@ -2,24 +2,25 @@
 using namespace std;
 
 int factorial(int num){
-    if (num < 0){
-        cout << "Error: Factorial of negative number is not defined.\n";
-        return -1;
-    }
-    else if (num == 0 || num == 1){
-        return 1;
-    }
-    else{
-        return num * factorial(num - 1);        
-    }
+   if (num < 0){
+       cout << "Error: Factorial of negative number is not defined.\n";
+       return -1;
+   }
+   else if (num == 0 || num == 1){
+       return 1;
+   }
+   else{
+       return num * factorial(num - 1);        
+   }
 }
 
-int main(){
-    int num;
-    cout << "Enter a number to calculate its factorial: ";
-    cin >> num;
-    int result = num * factorial(num-1);
-    printf("The factorial of %d! is %d", num, result);
-    return 0;
+int main() {
+   int num;
+   cout << "Enter a number to calculate its factorial: ";
+   cin >> num;
+   int result = factorial(num);
+   if (result != -1){
+       printf("The factorial of %d! is %d", num, result);
+   }
+   return 0;
 }
-
